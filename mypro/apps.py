36583a6e78ed6ad2a2056b7f8e10a916fbfe0e28
17607193257 <604784166@qq.com>
2019-12-21 20:51:26 +0800

    删除mypro子应用

--- mypro/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyproConfig(AppConfig):
-    name = 'mypro'
--- mypro/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from mypro.views import login
-
-urlpatterns = [
-    path('login/',login),
-]
